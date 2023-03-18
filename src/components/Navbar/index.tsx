6fbf20b24ce57d6eb4b72da8b019e69fd7339f7e
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2023-03-18 13:33:15 -0300

    limpo o codigo da navbar e agr tá responsiva

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,12 @@
+import { Flex, HStack, Text } from "@chakra-ui/react";
+import { Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
-import { Flex, HStack, IconButton, Stack, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
 import logo from "../../../public/img-logo.png";
 import { TextsNav } from "../TextsNav";
-import { Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 
 // responsividade:
 //     base     sm      md      lg      xl
@@ -14,7 +14,15 @@ import { Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 
 // como n estou em pages, n preciso botar o default aqui
 export function Navbar() {
-    const [displayatual, mudarDisplay] = useState('none');
+    const [iconAtual, mudarIcon] = useState(<HamburgerIcon color={'black'}/>);
+
+    const handleMouseUp = () => {
+        if (iconAtual.type === HamburgerIcon) {
+            mudarIcon(<CloseIcon color={"black"} />);
+        } else {
+            mudarIcon(<HamburgerIcon color={"black"} />);
+        }
+    };
 
     return (
         <HStack
@@ -25,7 +33,6 @@ export function Navbar() {
             width={'100%'}
             pl={'21px'}
             pr={'31px'}
-            // display={'column'}
         >
             <HStack
                 as='section'
@@ -56,22 +63,6 @@ export function Navbar() {
                 </Link>
             </Flex>
 
-            <IconButton
-                aria-label="Open Menu"
-                bg='#fadf98'
-                colorScheme={'none'}
-                size={'lg'}
-                mr={'2'}
-                icon={<HamburgerIcon color={'black'} />}
-                display={['flex', 'flex', 'flex', 'none', 'none']}
-                _active={{
-                    bg: '#e8ce87',
-                    transform: 'scale(0.98)',
-                    borderColor: '#e8ce87',
-                }}
-                onClick={() => mudarDisplay('flex')}
-            />
-
             <Menu>
                 <MenuButton 
                     as={Button} 
@@ -87,10 +78,11 @@ export function Navbar() {
                     margin={'0'}
                     h={'12'}
                     border={'2px solid #e8ce87'}
+                    onMouseUp={handleMouseUp}
                 >
-                    <HamburgerIcon color={'black'}/>
+                    {iconAtual}
                 </MenuButton>
-                <MenuList zIndex={20}>
+                <MenuList zIndex={20} display={['MenuList', 'MenuList', 'MenuList', 'none', 'none']}>
                     <MenuItem>
                         <Link href={"/sobre"}>
                             <TextsNav text={"Sobre Nós"} />
@@ -108,56 +100,6 @@ export function Navbar() {
                     </MenuItem>
                 </MenuList>
             </Menu>
-
-            <Flex
-                // w={'100vw'}
-                // h={'100vh'}
-                alignSelf={'center'}
-                height={'188px'}
-                width={'100%'}
-                bg='#F9E6B3'
-                zIndex={20}
-                pos={'fixed'}
-                top={'0'}
-                left={'0%'}
-                overflowY={'auto'}
-                flexDir={'column'}
-                display={[displayatual, displayatual, displayatual, 'none', 'none']}
-            >
-                <Flex justify={'flex-end'}>
-                    <IconButton 
-                        aria-label={"Close Menu"} 
-                        bg='#fadf98'
-                        colorScheme={'none'}
-                        size={'lg'}
-                        mt={'2'}
-                        mr={'2'}
-                        icon={<CloseIcon color={'black'} />}
-                        _active={{
-                            bg: '#e8ce87',
-                            transform: 'scale(0.98)',
-                            borderColor: '#e8ce87',
-                        }}
-                        onClick={() => mudarDisplay('none')}
-                    />
-                </Flex>
-                <Flex
-                    as='section'
-                    flexDirection={'column'}
-                    align={'center'}
-                    gap={'2'}
-                >
-                    <Link href={"/sobre"}>
-                        <TextsNav text={"Sobre Nós"} />
-                    </Link>
-                    <Link href={"/contato"}>
-                        <TextsNav text={"Contato"} />
-                    </Link>
-                    <Link href={"/login"}>
-                        <TextsNav text={"Login"} />
-                    </Link>
-                </Flex>
-            </Flex>
         </HStack>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,11 @@ import { theme } from '@/styles/theme'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
 
-import { Nav } from '@/components/Nav'
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // para pegar o theme da pasta styles
     <ChakraProvider  resetCSS theme={theme}> 
       <Navbar />
-      {/* <Nav /> */}
       <Component {...pageProps} />
       <Footer />
     </ChakraProvider>
